15ad4c5c01fd792fe96d18c1048cf54ee53cf67e
Asmn <ashminniraula78@gmail.com>
2023-02-24 20:36:10 +0545

    extract food items

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import csv
+import json
+
 url = 'https://www.daraz.com.np/catalog/?spm=a2a0e.11779170.search.1.58112d2bYOmFx7&q=food items&_keyori=ss&from=search_history&sugg=food items_0_1'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 script = soup.find("script", text=re.compile("window.pageData"))
+data = json.loads(script.string[16:])
 
-filename = 'output.txt'
-with open(filename, 'w') as f:
-    f.write(script.string)
+columns = ['item', 'unit', 'amount']
+with open ('items.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(columns)
+    for items in data['mods']['filter']['filterItems']:
+        if 'options' in items:
+            for a in items['options']:
+                writer.writerow([a['title'], '', ''])
